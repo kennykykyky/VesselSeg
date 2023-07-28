ff8939f022fa9c49803670482b0a4c3a17ee5b30
kennykyky <kennyzhang315cn@gmail.com>
2023-07-28 00:27:48 -0700

    fix some tiny issues

--- core/losses/cldiceLoss.py
+++ core/losses/cldiceLoss.py
@@ -46,8 +46,8 @@ class soft_cldice(nn.Module):
     def forward(self, y_true, y_pred):
         skel_pred = soft_skel(y_pred, self.iter)
         skel_true = soft_skel(y_true, self.iter)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,:,...])+self.smooth)/(torch.sum(skel_pred[:,:,...])+self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,:,...])+self.smooth)/(torch.sum(skel_true[:,:,...])+self.smooth)    
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,1:,...])+self.smooth)/(torch.sum(skel_pred[:,1:,...])+self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,1:,...])+self.smooth)/(torch.sum(skel_true[:,1:,...])+self.smooth)    
         cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
         return cl_dice
 
@@ -79,7 +79,7 @@ class soft_dice_cldice(nn.Module):
         dice = soft_dice(y_true, y_pred)
         skel_pred = soft_skel(y_pred, self.iter)
         skel_true = soft_skel(y_true, self.iter)
-        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,:,...])+self.smooth)/(torch.sum(skel_pred[:,:,...])+self.smooth)    
-        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,:,...])+self.smooth)/(torch.sum(skel_true[:,:,...])+self.smooth)    
+        tprec = (torch.sum(torch.multiply(skel_pred, y_true)[:,1:,...])+self.smooth)/(torch.sum(skel_pred[:,1:,...])+self.smooth)    
+        tsens = (torch.sum(torch.multiply(skel_true, y_pred)[:,1:,...])+self.smooth)/(torch.sum(skel_true[:,1:,...])+self.smooth)    
         cl_dice = 1.- 2.0*(tprec*tsens)/(tprec+tsens)
         return (1.0-self.alpha)*dice+self.alpha*cl_dice
